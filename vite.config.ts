6cdc243bbaf2ea1d3686d99290004021773b5b11
DmytroDar <kadf0123@gmail.com>
2025-12-06 22:27:03 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
-export default defineConfig(({ mode }) => ({
-  base: "/leonieofficial/",
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-  server: {
-    host: "::",
-    port: 8080,
-  },
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    base: "/leonieofficial/",
 
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+
+    plugins: [react(), mode === "development" && componentTagger()].filter(
+      Boolean
+    ),
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
